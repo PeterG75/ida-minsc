b16961975a78825175257a240a58c85f7c342297
Ali Rizvi-Santiago <arizvisa@gmail.com>
2018-09-11 18:44:03 -0500

    Reverted part of 63b4767d4941dd598faa264c791c375afeb6168b which added custom document names to the architecture operands namespaces. Now they're gone.

--- base/instruction.py
+++ base/instruction.py
@@ -1216,7 +1216,6 @@ del(operand_types)
 
 ## intel operands
 @document.namespace
-@document.rename('intel-operands')
 class intelops:
     """
     This internal namespace contains the different operand types that
@@ -1295,7 +1294,6 @@ class intelops:
 
 ## arm operands
 @document.namespace
-@document.rename('arm-operands')
 class armops:
     """
     This internal namespace contains the different operand types that
@@ -1410,7 +1408,6 @@ class armops:
 
 ## mips operands
 @document.namespace
-@document.rename('mips-operands')
 class mipsops:
     """
     This internal namespace contains the different operand types that
